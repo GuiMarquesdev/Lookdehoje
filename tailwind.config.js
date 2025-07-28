df4dc4ee4214725635153950519ffa84584286fd
guimarquesdev <guimarquesbrito@gmail.com>
2025-07-28 20:03:58 -0300

    primeiro comit do projeto look de hoje]

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Atenção aqui: certifique-se que o caminho aponta para seus arquivos
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
